9b6a1fef6e5392c82e6aba4250ae9edf7757d7e6
Makito <sumimakito@hotmail.com>
2024-05-17 12:51:14 +0800

    update

--- src/types.ts
+++ src/types.ts
@@ -11,11 +11,18 @@ export interface InputField extends Field<number | string> {
   placeholder?: string
 }
 
+/**
+ * SelectField is a field works like a dropdown that allow user to select a value.
+ */
 export interface SelectField<V> extends Field<V> {
   type: 'select'
-  options: string[]
+  options: string[] // What could we improve?
 }
 
+/**
+ * ArrayField is a field that allows user to add/edit/remove fields of a specific type.
+ * There will be only one specific field type in an array field.
+ */
 export interface ArrayField<E> extends Field<E[]> {
   items: Field<E>
 }
